55c1f9f3c753479d4badb561149007c80365ebb2
janenath <jane.e.nath@gmail.com>
2021-05-23 11:39:53 -0700

    Content and styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin, faMedium, faGithub } from '@fortawesome/free-brands-svg-icons'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">      
+      <h1>Jane Nath</h1>
+      <div className="description">
+        <p>full-stack software developer</p>
+        <p>former nurse midwife</p>
+      </div>
+      <div className="socialLinks">
+        <a className="email" href="mailto:jane.e.nath@gmail.com"><FontAwesomeIcon icon={faEnvelope} href="mailto:jane.e.nath@gmail.com"/></a>
+        <a className="linkedin" href="https://www.linkedin.com/in/janenath" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin}/></a>
+        <a className="github" href="https://www.github.com/janenath" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub}/></a>
+        <a className="medium" href="https://medium.com/@jane.e.nath" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faMedium}/></a>
+      </div>
     </div>
   );
 }
